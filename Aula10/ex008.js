9b96b9111bc23a806f2e7f1b962224ecdc3d3e78
João Paulo <jp_faria@id.uff.br>
2022-05-08 23:10:55 -0300

    novos exercicios javascript

---
+++ Aula10/ex008.js
@@ -0,0 +1,6 @@
+var vel=12
+console.log(`A velocidade do carro é ${vel} km/h`)
+if (vel>60){// condição simples!!
+     console.log(`Você passou o limite de velocidade, está multado`)
+}
+console.log(`Dirija com segurança`)
---
+++ Aula10/ex009.js
@@ -0,0 +1,6 @@
+var país ='Brasil'
+console.log (`Viendo em ${país} `)
+if(país =='Brasil'){console.log('Brasileiro!')}else {
+    console.log('Estrangeiro')
+}
+
