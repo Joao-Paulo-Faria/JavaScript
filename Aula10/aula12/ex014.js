b9a4cf3bdb2be8bc55b673060268f562aa54129f
Joao-Paulo-Faria <jp_faria@id.uff.br>
2022-05-12 19:24:33 -0300

    atualização

---
+++ Aula10/aula12/ex014.js
@@ -0,0 +1,33 @@
+var agora = new Date()
+//var diaSem = agora.getDay()
+console.log(diaSem)
+var diaSem = 8
+
+//switch usa para valores unitarios(pontuais)
+switch(diaSem){
+    case 0:
+        console.log('domingo')
+        break
+    case 1:
+        console.log('segunda')
+        break
+    case 2:
+        console.log('t')
+        break  
+    case 3:
+        console.log('q')
+        break 
+    case 4:
+        console.log('qui')
+        break 
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+    console.log('sabado')
+    break
+default:
+    console.log('dia inválido')
+    break
+}
+    
